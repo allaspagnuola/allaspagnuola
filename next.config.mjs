e97e784355ac9d988cb60af7af4d3776465bf22a
Siyi Zhou <sizhou2@student.unimelb.edu.au>
2025-03-23 16:42:53 +1100

    change deploy workflow environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports

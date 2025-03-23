f5ea7180df22f93263f7d84a3fa5607f640c8d9d
Siyi Zhou <sizhou2@student.unimelb.edu.au>
2025-03-23 16:31:01 +1100

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ try {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports
+  basePath: '/Portfolio',
   images: {
     unoptimized: true,
   },

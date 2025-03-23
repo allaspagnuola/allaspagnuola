50c81e3ad7de7f6e012d6c416d9228ae229e9e28
Siyi Zhou <sizhou2@student.unimelb.edu.au>
2025-03-23 21:20:58 +1100

    rollback

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  // change the title and description
+  title: 'Chloe.Z Portfolio',
+  description: 'Created by Chloe.Z',
+  generator: 'Chloe',
 }
 
 export default function RootLayout({

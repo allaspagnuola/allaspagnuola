34cfd6ea5e6878592cdacff9607b1756edae6e64
Siyi Zhou <sizhou2@student.unimelb.edu.au>
2025-04-01 20:22:35 +1100

    fix server images trial 3

--- next.config.mjs
+++ next.config.mjs
@@ -11,9 +11,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: false,
-  },
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
